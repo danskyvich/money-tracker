9b2ac1133e8ec76637d2fa7cc709230c0ac5583f
Danilo Pelin <danilocanabanopelinjr@gmail.com>
2026-09-03 21:51:53 +0800

    file: renamed `ProfilePage.tsx` to `profile-page.tsx` for naming consistency

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -1,4 +1,4 @@
-import ProfilePage from "@/features/profile/ProfilePage";
+import ProfilePage from "@/features/profile/profile-page";
 import { getUser, isUserMfaEnabled } from "@/lib/supabase/actions/auth";
 
 export default async function Profile() {
